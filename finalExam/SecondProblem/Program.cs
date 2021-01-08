e732982df09648e130fb070f8248e04df052cb12
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-08 15:49:33 +0200

    Added Second problem from fund. final exam

---
+++ finalExam/SecondProblem/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SecondProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Regex regex = new Regex(@"(!)(?<command>[A-Z][a-z]{2,})\1:\[(?<message>[A-z]{8,})\]");
+            for (int i = 0; i < n; i++)
+            {
+                string inputCommands = Console.ReadLine();
+                Match matches = regex.Match(inputCommands);
+                if (matches.Success)
+                {
+                    List<int> encription = new List<int>();
+                    string currMatch = matches.Groups["message"].Value;
+                    string currCommand = matches.Groups["command"].Value;
+                    foreach (var character in currMatch)
+                    {
+                        encription.Add((int) character);
+                    }
+
+                    Console.Write($"{currCommand}: ");
+                    Console.WriteLine(string.Join(" ", encription));
+                }
+                else
+                {
+                    Console.WriteLine("The message is invalid");
+                }
+            }
+        }
+    }
+}
