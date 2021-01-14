3a521f9b0823aa023c665b9721fbd1473f6d214a
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-14 21:15:59 +0200

    Added Supermarket problem

---
+++ StacksAndQueues/Supermarket/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualBasic;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> customers = new Queue<string>();
+            string people = Console.ReadLine();
+            while (people!="End")
+            {
+                if (people=="Paid")
+                {
+                    Console.WriteLine(String.Join(Environment.NewLine, customers));
+                    customers.Clear();
+                }
+                else
+                {
+                    customers.Enqueue(people);
+                }
+
+                people = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{customers.Count} people remaining.");
+        }
+    }
+}
