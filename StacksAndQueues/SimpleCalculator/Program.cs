3fd865c630d6de2af174e53f173f7f2ae9bad0be
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-14 19:42:32 +0200

    Added SimpleCalculator problem

---
+++ StacksAndQueues/SimpleCalculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> expression =
+                new Stack<string>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Reverse());
+            while (expression.Count > 1)
+            {
+                int num1 = int.Parse(expression.Pop());
+                string symbol = expression.Pop();
+                int num2 = int.Parse(expression.Pop());
+                if (symbol == "+")
+                {
+                    expression.Push((num1 + num2).ToString());
+                }
+                else
+                {
+                    expression.Push((num1 - num2).ToString());
+                }
+            }
+
+            Console.WriteLine(expression.Pop());
+        }
+    }
+}
