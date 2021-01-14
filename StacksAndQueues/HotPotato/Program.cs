f837425f62b90f7a4d77b1c707a5c640524327d2
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-14 21:28:22 +0200

    Added HotPotato problem

---
+++ StacksAndQueues/HotPotato/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> kids =
+                new Queue<string>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries));
+            int passes = int.Parse(Console.ReadLine());
+            while (kids.Count > 1)
+            {
+                for (int i = 0; i < passes; i++)
+                {
+                    if (i == passes - 1)
+                    {
+                        Console.WriteLine($"Removed {kids.Dequeue()}");
+                    }
+                    else
+                    {
+                        kids.Enqueue(kids.Dequeue());
+                    }
+                }
+            }
+
+            Console.WriteLine($"Last is {kids.Dequeue()}");
+        }
+    }
+}
