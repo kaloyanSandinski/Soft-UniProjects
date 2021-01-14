080d94518064b63ebd3a0c39bbda515dbe2e6051
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-14 19:54:21 +0200

    Added MatchingBrackets problem

---
+++ StacksAndQueues/MatchingBrackets/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStr = Console.ReadLine();
+            Stack<int> index = new Stack<int>();
+            for (int i = 0; i < inputStr.Length; i++)
+            {
+                if (inputStr[i]=='(')
+                {
+                    index.Push(i);
+                }
+                else if (inputStr[i]==')')
+                {
+                    int startIndex = index.Pop();
+                    Console.WriteLine(inputStr.Substring(startIndex, i-startIndex+1));
+                }
+            }
+        }
+    }
+}
