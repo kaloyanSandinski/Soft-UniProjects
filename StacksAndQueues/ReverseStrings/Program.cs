cf563e91eec8be01776d894c749bbf32030f9553
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-14 16:00:44 +0200

    Added new solution

---
+++ StacksAndQueues/ReverseStrings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+
+            while (stack.Count>0)
+            {
+                Console.Write(stack.Pop());
+            }*/
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+
+            foreach (var character in stack)
+            {
+                Console.Write(character);
+            }
+        }
+    }
+} 
