df7ce0d5f906c87f60d560e7e1298b4f83110fe7
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-15 18:52:05 +0200

    Added FastFood problem

---
+++ StacksAndQueues/FastFood/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+            int[] inputOrders = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> ordersQuantity = new Queue<int>(inputOrders);
+            Console.WriteLine(ordersQuantity.Max());
+            while (ordersQuantity.Any() && foodQuantity >= ordersQuantity.Peek())
+            {
+                foodQuantity -= ordersQuantity.Peek();
+                ordersQuantity.Dequeue();
+            }
+
+            if (ordersQuantity.Any())
+            {
+                Console.Write($"Orders left: ");
+                Console.WriteLine(String.Join(" ", ordersQuantity));
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
