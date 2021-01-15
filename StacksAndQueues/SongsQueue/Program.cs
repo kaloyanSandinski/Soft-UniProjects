3e06df39556b861f87214987e02424ae096bb016
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-15 20:36:01 +0200

    Added SongsQueue problem

---
+++ StacksAndQueues/SongsQueue/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SongsQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputSongs = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            Queue<string> songs = new Queue<string>(inputSongs);
+            string command = Console.ReadLine();
+            while (songs.Any())
+            {
+                if (command.Contains("Play"))
+                {
+                    songs.Dequeue();
+                }
+                else if (command.Contains("Add"))
+                {
+                    string songToAdd = command.Substring(4);
+                    if (songs.Contains(songToAdd))
+                    {
+                        Console.WriteLine($"{songToAdd} is already contained!");
+                    }
+                    else
+                    {
+                        songs.Enqueue(songToAdd);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(String.Join(", ", songs));
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
