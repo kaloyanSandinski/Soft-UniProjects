ab45db25f8050c5f0eb596aa084511e429e4bc7e
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-15 18:20:40 +0200

    Added MaximumAndMinimumElement problem

---
+++ StacksAndQueues/MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualBasic;
+
+namespace MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> stackedElements = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int[] queries = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                int command = queries[0];
+                if (command == 1)
+                {
+                    stackedElements.Push(queries[1]);
+                }
+                else if (command == 2)
+                {
+                    stackedElements.Pop();
+                }
+                else if (command == 3 && stackedElements.Any())
+                {
+                    Console.WriteLine(stackedElements.Max());
+                }
+                else if (command == 4 && stackedElements.Any())
+                {
+                    Console.WriteLine(stackedElements.Min());
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", stackedElements));
+        }
+    }
+}
