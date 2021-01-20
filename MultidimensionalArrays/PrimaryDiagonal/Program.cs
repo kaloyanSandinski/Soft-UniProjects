22918f14a1f18732ce2eb495141f03e9d505f98b
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-21 01:54:07 +0200

    Added new problem PrimaryDiagonal

---
+++ MultidimensionalArrays/PrimaryDiagonal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sizeMatrix = int.Parse(Console.ReadLine());
+            var matrix = new int[sizeMatrix, sizeMatrix];
+            var sumDiagonal = 0;
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                var inputArr = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[rows, cols] = inputArr[cols];
+                    if (rows == cols)
+                    {
+                        sumDiagonal += matrix[rows, cols];
+                    }
+                }
+            }
+
+            Console.WriteLine(sumDiagonal);
+        }
+    }
+}
