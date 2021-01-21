3c9062c33fafe8910036fc2d06e5722ddff17d6a
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-21 02:20:39 +0200

    Added new problem: SymbolInMatrix

---
+++ MultidimensionalArrays/SymbolInMatrix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrixDimentions = int.Parse(Console.ReadLine());
+            var matrix = new char[matrixDimentions, matrixDimentions];
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                var inputArr = Console.ReadLine()
+                    .ToString()
+                    .ToCharArray();
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    matrix[rows, cols] += inputArr[cols];
+                }
+            }
+
+            var symbol = char.Parse(Console.ReadLine());
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    if (matrix[rows, cols] == symbol)
+                    {
+                        Console.WriteLine($"({rows}, {cols})");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{symbol} does not occur in the matrix ");
+        }
+    }
+}
