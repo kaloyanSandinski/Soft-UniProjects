268d79b17a5b572b01a62e1baf75187a65abe8aa
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-21 14:48:25 +0200

    Added new problem: PascalTriangle and edit in: JaggedArrayModification

--- MultidimensionalArrays/JaggedArrayModification/Program.cs
+++ MultidimensionalArrays/JaggedArrayModification/Program.cs
@@ -8,17 +8,14 @@ namespace JaggedArrayModification
         static void Main(string[] args)
         {
             int matrixDimentions = int.Parse(Console.ReadLine());
-            var matrix = new int[matrixDimentions, matrixDimentions];
-            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            var matrix = new int[matrixDimentions][];
+            for (int rows = 0; rows < matrix.Length; rows++)
             {
                 var inputArr = Console.ReadLine()
                     .Split(' ', StringSplitOptions.RemoveEmptyEntries)
                     .Select(int.Parse)
                     .ToArray();
-                for (int cols = 0; cols < matrix.GetLength(1); cols++)
-                {
-                    matrix[rows, cols] = inputArr[cols];
-                }
+                matrix[rows] = inputArr;
             }
 
             var inputComands = Console.ReadLine()
@@ -29,15 +26,15 @@ namespace JaggedArrayModification
                 int row = int.Parse(inputComands[1]);
                 int col = int.Parse(inputComands[2]);
                 int value = int.Parse(inputComands[3]);
-                if (row >= 0 && col >= 0 && row < matrix.GetLength(0) && col < matrix.GetLength(1))
+                if (row >= 0 && col >= 0 && row < matrix.Length && col < matrix[row].Length)
                 {
                     if (inputComands[0] == "Add")
                     {
-                        matrix[row, col] += value;
+                        matrix[row][col] += value;
                     }
                     else
                     {
-                        matrix[row, col] -= value;
+                        matrix[row][col] -= value;
                     }
                 }
                 else
@@ -50,14 +47,9 @@ namespace JaggedArrayModification
                     .ToArray();
             }
 
-            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            foreach (var currArr in matrix)
             {
-                for (int cols = 0; cols < matrix.GetLength(1); cols++)
-                {
-                    Console.Write($"{matrix[rows, cols]} ");
-                }
-
-                Console.WriteLine();
+                Console.WriteLine(String.Join(" ", currArr));
             }
         }
     }
