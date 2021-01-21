7cd83dc87be2e9522150c20ca9a1692b2cd4d4b8
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-21 15:30:31 +0200

    Solved PascalTriangle problem

--- MultidimensionalArrays/PascalTriangle/Program.cs
+++ MultidimensionalArrays/PascalTriangle/Program.cs
@@ -6,7 +6,29 @@ namespace PascalTriangle
     {
         static void Main(string[] args)
         {
-            
+            int rows = int.Parse(Console.ReadLine());
+            int currLength = 1;
+            var triangle = new long[rows][];
+            for (int i = 0; i < triangle.Length; i++)
+            {
+                triangle[i] = new long[currLength];
+                triangle[i][0] = 1;
+                triangle[i][currLength - 1] = 1;
+                if (currLength>2)
+                {
+                    for (int j = 1; j < currLength-1; j++)
+                    {
+                        triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+                    }
+                }
+                
+                currLength++;
+            }
+
+            foreach (var row in triangle)
+            {
+                Console.WriteLine(string.Join(' ', row));
+            }
         }
     }
 }
