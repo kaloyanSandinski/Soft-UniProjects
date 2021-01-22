dd9659d11dca55af2a80071b8971fa12ca359a3f
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-22 02:21:46 +0200

    Added new problem: SnakeMoves

---
+++ MultidimensionalArrays/SnakeMoves/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace SnakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sizeMatrix = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            var snake = Console.ReadLine();
+            var matrix = new char[sizeMatrix[0], sizeMatrix[1]];
+            int counter = 0;
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                if (rows % 2 == 0)
+                {
+                    for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                    {
+                        if (counter >= snake.Length)
+                        {
+                            counter = 0;
+                        }
+
+                        matrix[rows, cols] = snake[counter];
+                        counter++;
+                    }
+                }
+                else
+                {
+                    for (int cols = matrix.GetLength(1) - 1; cols >= 0; cols--)
+                    {
+                        if (counter >= snake.Length)
+                        {
+                            counter = 0;
+                        }
+
+                        matrix[rows, cols] = snake[counter];
+                        counter++;
+                    }
+                }
+            }
+
+            for (int rows = 0; rows < matrix.GetLength(0); rows++)
+            {
+                for (int cols = 0; cols < matrix.GetLength(1); cols++)
+                {
+                    Console.Write(matrix[rows, cols]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
