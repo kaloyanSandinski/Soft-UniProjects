ff4926296ab0555d866b2a8e6132b5e967574103
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-22 22:10:07 +0200

    Changes in: .DS_Store, added new solution: SetsAndDictionaries and added new problem: CountSameValuesInArray

---
+++ SetsAndDictionaries/CountSameValuesInArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputValues = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+            Dictionary<double, int> countOfElements = new Dictionary<double, int>();
+            for (int i = 0; i < inputValues.Length; i++)
+            {
+                if (countOfElements.ContainsKey(inputValues[i]))
+                {
+                    countOfElements[inputValues[i]]++;
+                }
+                else
+                {
+                    countOfElements.Add(inputValues[i], 1);
+                }
+            }
+
+            foreach (var currValue in countOfElements)
+            {
+                Console.WriteLine($"{currValue.Key} - {currValue.Value} times");
+            }
+        }
+    }
+}
