27784866c303d9eb06f58ac78145361caa976be5
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-23 18:20:19 +0200

    Added new problem: CitiesByContinentAndCountry

---
+++ SetsAndDictionaries/CitiesByContinentAndCountry/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CitiesByContinentAndCountry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCountries = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, List<string>>> allCountries =
+                new Dictionary<string, Dictionary<string, List<string>>>();
+            for (int i = 0; i < numberOfCountries; i++)
+            {
+                var currCountry = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                var continent = currCountry[0];
+                var country = currCountry[1];
+                var city = currCountry[2];
+                if (allCountries.ContainsKey(continent))
+                {
+                    if (allCountries[continent].ContainsKey(country))
+                    {
+                        allCountries[continent][country].Add(city);
+                    }
+                    else
+                    {
+                        allCountries[continent].Add(country, new List<string>());
+                        allCountries[continent][country].Add(city);
+                    }
+                    
+                }
+                else
+                {
+                    allCountries.Add(continent, new Dictionary<string, List<string>>());
+                    allCountries[continent].Add(country, new List<string>());
+                    allCountries[continent][country].Add(city);
+                }
+            }
+
+            foreach (var currContinent in allCountries)
+            {
+                Console.WriteLine($"{currContinent.Key}:");
+                foreach (var currCountry in currContinent.Value)
+                {
+                    Console.WriteLine($"{currCountry.Key} -> {string.Join(", ", currCountry.Value)}");
+                }
+            }
+        }
+    }
+}
