ff1c01060f14df9df142d6431e6804c97d46f272
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-23 18:28:23 +0200

    Added new problem: RecordUniqueNames

---
+++ SetsAndDictionaries/RecordUniqueNames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecordUniqueNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNames = int.Parse(Console.ReadLine());
+            HashSet<string> setOfUniqueNames = new HashSet<string>();
+            for (int i = 0; i < inputNames; i++)
+            {
+                var currName = Console.ReadLine();
+                setOfUniqueNames.Add(currName);
+            }
+
+            foreach (var currName in setOfUniqueNames)
+            {
+                Console.WriteLine(currName);
+            }
+        }
+    }
+}
