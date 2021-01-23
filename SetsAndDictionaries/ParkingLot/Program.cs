8fca2056e992ed5a24802de0a77043c3c550540a
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-23 18:39:41 +0200

    Added new problem: ParkingLot

---
+++ SetsAndDictionaries/ParkingLot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tokens = Console.ReadLine();
+            HashSet<string> carsInParking = new HashSet<string>();
+            while (tokens!="END")
+            {
+                var inputCmnd = tokens
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                var direction = inputCmnd[0];
+                var carNumber = inputCmnd[1];
+                if (direction=="IN")
+                {
+                    carsInParking.Add(carNumber);
+                }
+                else
+                {
+                    carsInParking.Remove(carNumber);
+                }
+
+                tokens = Console.ReadLine();
+            }
+
+            if (carsInParking.Any())
+            {
+                foreach (var currCar in carsInParking)
+                {
+                    Console.WriteLine(currCar);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
