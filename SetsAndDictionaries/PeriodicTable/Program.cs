4d65c53f9285f41b5e2dd16d14bbede126c6cce9
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-24 02:22:16 +0200

    Added new problem: PeriodicTable

---
+++ SetsAndDictionaries/PeriodicTable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            SortedSet<string>uniqueElements = new SortedSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                var inputLines = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                foreach (var currElement in inputLines)
+                {
+                    uniqueElements.Add(currElement);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", uniqueElements));
+        }
+    }
+}
