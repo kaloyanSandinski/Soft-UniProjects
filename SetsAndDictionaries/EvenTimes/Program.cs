0f9ac0df00f16674c1f9403ab2536fea06e63ac3
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-24 02:35:44 +0200

    Added new problem: EvenTimes

---
+++ SetsAndDictionaries/EvenTimes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<int, int> allNumbers = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                int currInput = int.Parse(Console.ReadLine());
+                if (allNumbers.ContainsKey(currInput))
+                {
+                    allNumbers[currInput]++;
+                }
+                else
+                {
+                    allNumbers.Add(currInput, 1);
+                }
+            }
+
+            foreach (var currNum in allNumbers)
+            {
+                if (currNum.Value%2==0)
+                {
+                    Console.WriteLine(currNum.Key);
+                }
+            }
+        }
+    }
+}
