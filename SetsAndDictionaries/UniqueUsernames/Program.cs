ad56ab2d6511f130d863b8366d2b22b5f5e64f5f
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-24 02:03:49 +0200

    Added new problem: UniqueUsernames

---
+++ SetsAndDictionaries/UniqueUsernames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> uniqueNames = new HashSet<string>();
+            for (int i = 0; i < n; i++)
+            {
+                uniqueNames.Add(Console.ReadLine());
+            }
+
+            foreach (var currName in uniqueNames)
+            {
+                Console.WriteLine(currName);
+            }
+        }
+    }
+}
