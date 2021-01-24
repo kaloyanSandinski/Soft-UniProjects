fc21fa244519112ffa845a75fae1bb06ce59f3e9
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-24 03:07:50 +0200

    Addded new problem: CountSymbols

---
+++ SetsAndDictionaries/CountSymbols/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputText = Console.ReadLine().ToCharArray();
+            Dictionary<char, int> allCharactersInText = new Dictionary<char, int>();
+            foreach (var currCharacter in inputText)
+            {
+                if (allCharactersInText.ContainsKey(currCharacter))
+                {
+                    allCharactersInText[currCharacter]++;
+                }
+                else
+                {
+                    allCharactersInText.Add(currCharacter,1);
+                }
+            }
+
+            allCharactersInText = allCharactersInText.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+            foreach (var currChar in allCharactersInText)
+            {
+                Console.WriteLine($"{currChar.Key}: {currChar.Value} time/s");
+            }
+        }    
+    }
+}
