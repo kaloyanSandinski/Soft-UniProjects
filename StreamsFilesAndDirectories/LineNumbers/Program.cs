29125f6a70dec46a201e8e2e47e59c9f2b0cd343
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-27 18:59:30 +0200

    Added new problem: LineNumbers

---
+++ StreamsFilesAndDirectories/LineNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine("data", "Input.txt");
+            string dest = Path.Combine("data", "Output.txt");
+            using (TextReader reader = new StreamReader(path))
+            {
+                using (TextWriter writer = new StreamWriter(dest))
+                {
+                    string currline = reader.ReadLine();
+                    int counter = 1;
+                    while (currline!=null)
+                    {
+                        writer.WriteLine($"{counter}. {currline}");
+                        counter++;
+                        currline = reader.ReadLine();
+                    } 
+                }
+            }
+        }
+    }
+}
