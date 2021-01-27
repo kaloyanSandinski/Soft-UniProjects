d5f025536d4c80f0d7c2a0a4e31b4d0b677b605a
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-28 01:47:36 +0200

    Added new problem: FolderSize

---
+++ StreamsFilesAndDirectories/FolderSize/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace FolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryName = Console.ReadLine();
+            string[] files =
+                Directory.GetFiles(directoryName);
+            double size = 0;
+            foreach (var file in files)
+            {
+                FileInfo info = new FileInfo(file);
+                size += info.Length;
+            }
+
+            Console.WriteLine(size/1024/1024);
+        }
+    }
+}
