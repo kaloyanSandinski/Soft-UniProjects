fb5411e370b571622c14db7152a1d742d836de17
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-27 22:38:53 +0200

    Added new problem: MergeFiles

---
+++ StreamsFilesAndDirectories/MergeFiles/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace MergeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstFile = Path.Combine("data", "FileOne.txt");
+            var secondFile = Path.Combine("data", "FileTwo.txt");
+            var outputFile = Path.Combine("data", "OutputFile.txt");
+            var firstText = File.ReadAllLines(firstFile);
+            var secondText = File.ReadAllLines(secondFile);
+            using (TextWriter outFile = new StreamWriter(outputFile))
+            {
+                for (int i = 0; i < firstText.Length; i++)
+                {
+                    outFile.WriteLine(firstText[i]);
+                    outFile.WriteLine(secondText[i]);
+                }
+            }
+        }
+    }
+}
