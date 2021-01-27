4e4ddd3666cd807512b9156157b7e1d7fde6ef39
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-27 21:07:04 +0200

    Added new problem: WordCount

---
+++ StreamsFilesAndDirectories/WordCount/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordsPath = Path.Combine("data", "words.txt");
+            string inputPath = Path.Combine("data", "text.txt");
+            string[] token = File.ReadAllText(wordsPath)
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            var elementsToSplit = new char[] {'.', ' ', '?', '!', '-',','};
+            var text = File.ReadAllText(inputPath)
+                .ToLower()
+                .Split(elementsToSplit, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            Dictionary<string, int> wordsCount = new Dictionary<string, int>();
+            
+            foreach (var currWord in token)
+            {
+                int counter = 0;
+                foreach (var word in text)
+                {
+                    if (word==currWord)
+                    {
+                        counter++;
+                    }
+                }
+                wordsCount.Add(currWord,counter);
+            }
+
+            foreach (var words in wordsCount)
+            {
+                Console.WriteLine($"{words.Key} - {words.Value}");
+            }
+        }
+    }
+}
