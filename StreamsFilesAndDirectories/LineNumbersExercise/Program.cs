6ed76b48b6ca3a234741326db2a58ff9940bb360
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-28 13:27:05 +0200

    Added new problem: LineNumbersExercise

---
+++ StreamsFilesAndDirectories/LineNumbersExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbersExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.Combine("data", "text.txt");
+            var destination = Path.Combine("data", "output.txt");
+            var lines = File.ReadAllLines(path);
+            for (int i = 1; i <= lines.Length; i++)
+            {
+                int characters = 0;
+                int symbols = 0;
+                foreach (var character in lines[i - 1])
+                {
+                    if (character == '-' || character == '!' || character == ',' || character == '.' ||
+                        character == '?' || character == '\'')
+                    {
+                        symbols++;
+                    }
+                    else if (character!=' ')
+                    {
+                        characters++;
+                    }
+                }
+
+                lines[i - 1] = $"Line {i}: {lines[i - 1]} ({characters})({symbols})";
+            }
+
+            File.WriteAllLines(destination, lines);
+        }
+    }
+}
