af3f54b3a764a494e8ec656c38f20a63268041ce
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-30 21:52:35 +0200

    Added new problem: AddVAT

---
+++ FunctionalProgramming/AddVAT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => double.Parse(x) * 1.2D)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"{x:f2}"));
+        }
+    }
+}
