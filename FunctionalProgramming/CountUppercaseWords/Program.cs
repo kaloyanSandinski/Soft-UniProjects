5212cc19388aacdfa4ae4bb91c5934fccb2cbe97
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-30 20:13:58 +0200

    Added new problem: CountUppercaseWords

---
+++ FunctionalProgramming/CountUppercaseWords/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> checker = x => x[0] == x.ToUpper()[0];
+            var words = Console.ReadLine()
+                .Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .Where(checker)
+                .ToArray();
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
