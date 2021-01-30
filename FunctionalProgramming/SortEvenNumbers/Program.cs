7752aec20d24eeffc2c68331ab59e5602b33537c
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-30 11:21:15 +0200

    Added new solution: FunctionalProgramming and new problem: SortEvenNumbers

---
+++ FunctionalProgramming/SortEvenNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine()
+                .Split(new string[] {", "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(n => int.Parse(n))
+                .Where(n=>n%2==0)
+                .OrderBy(n=>n)
+                .ToArray();
+            Console.WriteLine(string.Join(", ", nums));
+        }
+    }
+}
