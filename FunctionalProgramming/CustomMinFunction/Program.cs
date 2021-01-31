bf3ffad700c160b93fe726e4815caac3b12b2b26
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-31 12:52:32 +0200

    Added new problem: CustomMinFunction

---
+++ FunctionalProgramming/CustomMinFunction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> myParse = s => int.Parse(s);
+            var inoutNums = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(myParse)
+                .ToArray();
+            Func<int[], int> MinFunc = nums =>
+            {
+                int min = int.MaxValue;
+                foreach (var currNum in nums)
+                {
+                    if (currNum < min)
+                    {
+                        min = currNum;
+                    }
+                }
+
+                return min;
+            };
+            Console.WriteLine(MinFunc(inoutNums));
+        }
+    }
+}
