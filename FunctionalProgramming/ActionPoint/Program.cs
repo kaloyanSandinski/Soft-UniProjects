df2156844390124d88d717c2c1e0c617c7895a68
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-31 12:38:06 +0200

    Added two new problems: ActionPoint and KnightsOfHonor

---
+++ FunctionalProgramming/ActionPoint/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .ToArray();
+            Action<string> printName = n => Console.WriteLine(n);
+            foreach (var currName in input)
+            {
+                printName(currName);
+            }
+        }
+    }
+}
---
+++ FunctionalProgramming/KnightsOfHonor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNames = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Action<string> print = n => Console.WriteLine($"Sir {n}");
+            foreach (var currName in inputNames)
+            {
+                print(currName);
+            }
+        }
+    }
+}
