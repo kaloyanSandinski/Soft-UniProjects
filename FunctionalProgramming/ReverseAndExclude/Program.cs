64162b6d181e81f8f99822b19de4a47b671a3626
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-01-31 21:50:00 +0200

    Added new problem: ReverseAndExclude

---
+++ FunctionalProgramming/ReverseAndExclude/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int num = int.Parse(Console.ReadLine());
+            List<int> output = input
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x))
+                .Where(x => x % num != 0)
+                .Reverse()
+                .ToList();
+            Printer(output);
+        }
+
+        public static void Printer(List<int> output)
+        {
+            Console.WriteLine(string.Join(' ', output));
+        }
+    }
+}
