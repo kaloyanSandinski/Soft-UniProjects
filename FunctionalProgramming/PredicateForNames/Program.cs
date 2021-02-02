e3b3defa862b7187ca10aaa1b83b8b6537af651f
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-02-02 12:01:55 +0200

    Added new problem: PredicateForNames

---
+++ FunctionalProgramming/PredicateForNames/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x.Length <= n)
+                .ToList()
+                .ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
