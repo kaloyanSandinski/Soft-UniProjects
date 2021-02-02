9abb910c9995ea699971b65dba330386870940c1
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-02-02 12:30:03 +0200

    Added new problem: CustomComparator

---
+++ FunctionalProgramming/CustomComparator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualBasic;
+
+namespace CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNums = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Array.Sort(inputNums, (a, b) =>
+            {
+                if (a % 2 != 0 && b % 2 == 0)
+                {
+                    return 1;
+                }
+                else if (a % 2 == 0 && b % 2 != 0)
+                {
+                    return -1;
+                }
+
+                int temp = a.CompareTo(b);
+                return temp;
+            });
+            Console.WriteLine(string.Join(" ", inputNums));
+        }
+    }
+}
