66ca2804c637fa8d96afb23497b45b3a2ff50e6d
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-02-02 13:17:23 +0200

    Added new problem: ListOfPredicates

---
+++ FunctionalProgramming/ListOfPredicates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var inputDeviders = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            Func<int, int, bool> predicate = (num, d) => { return num % d == 0; };
+            for (int i = 1; i <= n; i++)
+            {
+                if (inputDeviders.All(d => predicate(i, d)))
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
