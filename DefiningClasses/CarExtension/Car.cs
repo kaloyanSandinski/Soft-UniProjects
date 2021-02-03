217af7a1c4bbb77a230cab1fb57b2c650c668247
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-02-03 14:44:58 +0200

    Added new problem: CarExtension

---
+++ DefiningClasses/CarExtension/Car.cs
@@ -0,0 +1,65 @@
+using System;
+
+namespace CarExtension
+{
+    public class Car
+    {
+        private string make;
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private int year;
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        private double fuelQuantity;
+
+        public double FuelQuantity
+        {
+            get { return this.fuelQuantity; }
+            set { this.fuelQuantity = value; }
+        }
+
+        private double fuelConsumption;
+
+        public double FuelConsumption
+        {
+            get { return this.fuelConsumption; }
+            set { this.fuelConsumption = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            double result = fuelQuantity - (distance * fuelConsumption);
+            if (result>0)
+            {
+                fuelQuantity -= result;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+        }
+    }
+}
