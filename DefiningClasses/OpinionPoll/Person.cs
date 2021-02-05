a342bb28e9fd4139b25982af0e716c03c128a1b4
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-02-05 22:43:34 +0200

    Edited OpinionPoll problem

---
+++ DefiningClasses/OpinionPoll/Person.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ DefiningClasses/OpinionPoll/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split().ToArray();
+                Person person = new Person(input[0], int.Parse(input[1]));
+                persons.Add(person);
+            }
+            persons = persons.OrderBy(x=>x.Name).ToList();
+
+            foreach (var currPerson in persons)
+            {
+                if (currPerson.Age>30)
+                {
+                    Console.WriteLine($"{currPerson.Name} - {currPerson.Age}");
+                }
+            }
+        }
+    }
+}
