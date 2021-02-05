07109b3c2b10456e47ea62e982d24a3ab7e4609d
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-02-05 18:09:26 +0200

    Added new problem: CreatingConstructors

---
+++ DefiningClasses/CreatingConstructors/Person.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+}
