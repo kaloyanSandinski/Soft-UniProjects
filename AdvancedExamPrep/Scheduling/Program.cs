5592150e0681cbc11a8047963951d095fe5c0e5b
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-02-14 17:43:13 +0200

    Edited Scheduling problem

--- AdvancedExamPrep/Scheduling/Program.cs
+++ AdvancedExamPrep/Scheduling/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Scheduling
 {
@@ -6,7 +8,49 @@ namespace Scheduling
     {
         static void Main(string[] args)
         {
-            
+            Stack<int> tasks = new Stack<int>(Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray());
+            Queue<int> threads = new Queue<int>(Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray());
+            int taskToBeKilled = int.Parse(Console.ReadLine());
+            int removedThread = 0;
+            while (tasks.Contains(taskToBeKilled) && threads.Any())
+            {
+                int currThread = threads.Peek();
+                int currTask = tasks.Peek();
+                if (currThread >= currTask)
+                {
+                    if (currTask == taskToBeKilled)
+                    {
+                        removedThread = threads.Peek();
+                        tasks.Pop();
+                    }
+                    else
+                    {
+                        threads.Dequeue();
+                        tasks.Pop();
+                    }
+                }
+                else
+                {
+                    if (currTask == taskToBeKilled)
+                    {
+                        tasks.Pop();
+                        removedThread = threads.Peek();
+                    }
+                    else
+                    {
+                        threads.Dequeue();
+                    }
+                }
+            }
+
+            Console.WriteLine($"Thread with value {removedThread} killed task {taskToBeKilled}");
+            Console.Write(string.Join(" ", threads));
         }
     }
 }
