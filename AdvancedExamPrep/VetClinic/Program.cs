f01be794e9b7740a74777bd46a42aecd5724f442
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-02-18 10:54:16 +0200

    Added new problem: VetClinic

---
+++ AdvancedExamPrep/VetClinic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VetClinic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
