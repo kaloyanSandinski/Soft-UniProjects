d4de4fb4e25d31a516cc0d0c39997cd9dee2617e
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-03-02 17:51:49 +0200

    Edited MultipleInheritance problem

---
+++ Inheritance/MultipleInheritance/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Puppy puppy = new Puppy();
+            Dog dog = new Dog();
+            puppy.Weep();
+            puppy.Bark();
+            puppy.Eat();
+            dog.Bark();
+            dog.Eat();
+        }
+    }
+}
