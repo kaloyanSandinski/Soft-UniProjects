eb452f8b64804c4981324340c1fd2d082587dc46
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-03-02 18:51:55 +0200

    Edited: StackOfStrings problem

---
+++ Inheritance/StackOfStrings/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+         StackOfStrings stack = new StackOfStrings();
+         Console.WriteLine(stack.IsEmpty());
+         stack.AddRange(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList());
+         foreach (var element in stack)
+         {
+             Console.WriteLine(element);
+         }
+        }
+    }
+}
