3e7d52de1d8718cf6bcf55ea7baab82d181fa84b
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-03-02 18:19:30 +0200

    Edited RandomList problem

---
+++ Inheritance/RandomList/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("az sam Pesho");
+            list.Add("az sam Gogi");
+            list.Add("az ne znam koi sam");
+            string randomItem = list.RandomString();
+            Console.WriteLine(randomItem);
+        }
+    }
+}
