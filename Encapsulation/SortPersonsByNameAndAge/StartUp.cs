61a59c8a25c33ebee0130ba95fa92f60af1ac50e
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-03-06 17:08:25 +0200

    Edited: SortPersonsByNameAndAge problem

--- Encapsulation/SortPersonsByNameAndAge/StartUp.cs
+++ Encapsulation/SortPersonsByNameAndAge/StartUp.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace SortPersonsByNameAndAge
+namespace PersonsInfo
 {
-    class Program
+    public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+                persons.Add(person);
+            }
+
+            persons.OrderBy(x=>x.FirstName)
+                .ThenBy(x=>x.Age)
+                .ToList()
+                .ForEach(x=> Console.WriteLine(x));
         }
     }
 }
