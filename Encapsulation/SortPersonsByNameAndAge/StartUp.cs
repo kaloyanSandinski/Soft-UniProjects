f8345bd30fdabea4d9fb67d6a2d23e947246ab04
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-03-06 15:12:55 +0200

    Added new solution: Encapsulation and new project: SortPersonsByNameAndAge

---
+++ Encapsulation/SortPersonsByNameAndAge/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SortPersonsByNameAndAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
