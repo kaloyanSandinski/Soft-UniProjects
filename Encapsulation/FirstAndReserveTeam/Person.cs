4e760fa3d9ab29bacdcd3d75fce747cb272b9384
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-03-07 19:33:08 +0200

    Added new problem: FirstAndReserveTeam

---
+++ Encapsulation/FirstAndReserveTeam/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public Person(string firstName, string lastName, int age, decimal salary) : this(firstName, lastName, age)
+        {
+            Salary = salary;
+        }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age <= 30)
+            {
+                this.Salary += this.Salary * percentage / 200;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+        }
+    }
+}
