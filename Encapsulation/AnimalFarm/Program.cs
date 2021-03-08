11408489102cabad65168b3cb595bd8a6c07b67d
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-03-08 13:47:42 +0200

    Edited: AnimalFarn problem

--- Encapsulation/AnimalFarm/Program.cs
+++ Encapsulation/AnimalFarm/Program.cs
@@ -1,20 +1,28 @@
 ﻿using System;
+using AnimalFarm.Models;
 
 namespace AnimalFarm
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             string name = Console.ReadLine();
             int age = int.Parse(Console.ReadLine());
 
-            Chicken chicken = new Chicken(name, age);
-            Console.WriteLine(
-                "Chicken {0} (age {1}) can produce {2} eggs per day.",
-                chicken.Name,
-                chicken.Age,
-                chicken.ProductPerDay);
+            try
+            {
+                Chicken chicken = new Chicken(name, age);
+                Console.WriteLine(
+                    "Chicken {0} (age {1}) can produce {2} eggs per day.",
+                    chicken.Name,
+                    chicken.Age,
+                    chicken.ProductPerDay);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
