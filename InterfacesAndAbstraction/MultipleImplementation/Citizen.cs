174c3d4f6ca8438e93ffa01c877cabb6e480bae0
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-03-12 11:44:37 +0200

    Edited: MultipleImplementation problem

---
+++ InterfacesAndAbstraction/MultipleImplementation/Citizen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IBirthable, IIdentifiable, IPerson
+    {
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+            Birthdate = birthdate;
+        }
+        public string Birthdate { get; set; }
+        public string Id { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+}
