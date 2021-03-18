6eda69802f3b62764f256b1bc30221fced92fd10
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-03-18 19:00:20 +0200

    Added new solution: ExceptionHandling and new problem: SquareRoot

---
+++ ExceptionHandling/SquareRoot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n<0)
+                {
+                    throw new Exception();
+                }
+
+                Console.WriteLine(Math.Sqrt(n));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
