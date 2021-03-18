b95c472319b8225599d6e4a1630280b84a7dd70e
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-03-18 19:42:23 +0200

    Edited: Fixing problem

--- ExceptionHandling/Fixing/Program.cs
+++ ExceptionHandling/Fixing/Program.cs
@@ -6,7 +6,25 @@ namespace Fixing
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var weekdays = new string[5];
+                weekdays[0] = "Sunday";
+                weekdays[1] = "Monday";
+                weekdays[2] = "Tuesday";
+                weekdays[3] = "Wednesday";
+                weekdays[4] = "Thursday";
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine(weekdays[i].ToString());
+                }
 
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
