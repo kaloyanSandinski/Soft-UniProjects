9f06410768675039663b4e0073afe3cad8985b55
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-03-18 19:52:45 +0200

    Edited: FixingVol2 problem

--- ExceptionHandling/FixingVol2/Program.cs
+++ ExceptionHandling/FixingVol2/Program.cs
@@ -2,11 +2,25 @@
 
 namespace FixingVol2
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1, num2;
+            byte result;
+
+            num1 = 30;
+            num2 = 60;
+            try
+            {
+                result = Convert.ToByte(num1 * num2);
+                Console.WriteLine($"{num1} * {num2} = {result}");
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
