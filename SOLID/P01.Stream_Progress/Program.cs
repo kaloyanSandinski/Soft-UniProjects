e9e5be2db9169c3d74604d2a627caae221bbdb6b
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-03-20 18:35:15 +0200

    Added two projects

---
+++ SOLID/P01.Stream_Progress/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml;
+
+namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        static void Main()
+        {
+            File file = new File("pesho", 50, 10);
+            Console.WriteLine(file.BytesSent);
+            Console.WriteLine(file.Length);
+        }
+    }
+}
---
+++ SOLID/P02.Graphic_Editor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P02.Graphic_Editor
+{
+    class Program
+    {
+        static void Main()
+        {
+            IShape square = new Square();
+            GraphicEditor graphic = new GraphicEditor();
+            graphic.DrawShape(square);
+            IShape circle = new Circle();
+            graphic.DrawShape(circle);
+        }
+    }
+}
