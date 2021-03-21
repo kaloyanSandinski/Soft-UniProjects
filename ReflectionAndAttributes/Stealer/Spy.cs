675829b13f27c0fbb566dacbb30b477c33da5c4f
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-03-21 20:43:03 +0200

    Edited: Stealer problem

---
+++ ReflectionAndAttributes/Stealer/Spy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string nameOfClass, string[] nameOfFieldsToInvestigate)
+        {
+            StringBuilder sb = new StringBuilder();
+            Type hackerType = typeof(Hacker);
+            FieldInfo[] allFields = hackerType.GetFields(
+                BindingFlags.Public |
+                BindingFlags.NonPublic| 
+                BindingFlags.Static |
+                BindingFlags.Instance);
+            object classInstance = Activator.CreateInstance(hackerType);
+            sb.AppendLine($"Class under investigation: {hackerType.Name}");
+            foreach (var currField in allFields.Where(x=>nameOfFieldsToInvestigate.Contains(x.Name)))
+            {
+                sb.AppendLine($"{currField.Name} = {currField.GetValue(classInstance)}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
