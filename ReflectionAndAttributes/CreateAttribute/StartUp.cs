d38572cc6d868c84cd50d118f6cc1ba6f8ad5ca5
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-03-23 12:08:24 +0200

    Edited: CreateAttribute problem

--- ReflectionAndAttributes/CreateAttribute/StartUp.cs
+++ ReflectionAndAttributes/CreateAttribute/StartUp.cs
@@ -2,11 +2,15 @@
 
 namespace AuthorProblem
 {
+    [Author("Vesko")]
+    [Author("Misho")]
     public class StartUp
     {
+        [Author("Pesho")]
         static void Main(string[] args)
         {
-            
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
         }
 
     }
